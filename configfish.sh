60ba94944b519daf92cbe9f2320eb22a7bd2ff22
Mike Wallio <michael.wallio@outlook.com>
2024-09-10 13:56:57 -0400

    Use function names as file names

--- configfish.sh
+++ configfish.sh
@@ -11,62 +11,61 @@ echo 'test -f $HOME/.cargo/env.fish && source $HOME/.cargo/env.fish' > $HOME/.co
 
 echo 'command -sq zoxide && zoxide init fish | source' > $HOME/.config/fish/conf.d/zoxide.fish
 
-echo '
-function dotnet
+echo 'function dotnet
     if test -f /usr/local/bin/run-dotnet.sh
         /usr/local/bin/run-dotnet.sh $argv
     else
         command dotnet $argv
     end
-end
+end' > $HOME/.config/fish/functions/dotnet.fish
 
-function nuget
+echo 'function nuget
     if test -f /usr/local/bin/run-nuget.sh
         /usr/local/bin/run-nuget.sh $argv
     else
         command nuget $argv
     end
-end
+end' > $HOME/.config/fish/functions/nuget.fish
 
-function npm
+echo 'function npm
     if test -f /usr/local/bin/run-npm.sh
         /usr/local/bin/run-npm.sh $argv
     else
         command npm $argv
     end
-end
+end' > $HOME/.config/fish/functions/npm.fish
 
-function yarn
+echo 'function yarn
     if test -f /usr/local/bin/run-yarn.sh
         /usr/local/bin/run-yarn.sh $argv
     else
         command yarn $argv
     end
-end
+end' > $HOME/.config/fish/functions/yarn.fish
 
-function npx
+echo 'function npx
     if test -f /usr/local/bin/run-npx.sh
         /usr/local/bin/run-npx.sh $argv
     else
         command npx $argv
     end
-end
+end' > $HOME/.config/fish/functions/npx.fish
 
-function rush
+echo 'function rush
     if test -f /usr/local/bin/run-rush.sh
         /usr/local/bin/run-rush.sh $argv
     else
         command rush $argv
     end
-end
+end' > $HOME/.config/fish/functions/rush.fish
 
-function rush-pnpm
+echo 'function rush-pnpm
     if test -f /usr/local/bin/run-rush-pnpm.sh
         /usr/local/bin/run-rush-pnpm.sh $argv
     else
         command rush-pnpm $argv
     end
-end' > $HOME/.config/fish/conf.d/codespace.fish
+end' > $HOME/.config/fish/functions/rush-pnpm.fish
 
 fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher scaryrawr/tide jorgebucaran/autopair.fish patrickf1/fzf.fish scaryrawr/fish-eza </dev/null"
 fish --command="set -Ux EZA_STANDARD_OPTIONS --icons"
