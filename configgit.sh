395001f5925112fb02858671d2a1affc3a7ec46c
Mike Wallio <michael.wallio@outlook.com>
2024-09-25 17:19:24 -0400

    Add missing `-d`

--- configgit.sh
+++ configgit.sh
@@ -6,7 +6,7 @@ git config --global core.editor "$CODE"
 git config --global merge.tool vscode
 git config --global mergetool.vscode.cmd "$CODE --merge \$REMOTE \$LOCAL \$BASE \$MERGED"
 git config --global diff.tool vscode
-git config --global difftool.vscode.cmd "$CODE \$LOCAL \$REMOTE"
+git config --global difftool.vscode.cmd "$CODE -d \$LOCAL \$REMOTE"
 git config --global alias.co checkout
 git config --global push.autoSetupRemote true
 git config --global grep.lineNumber true
