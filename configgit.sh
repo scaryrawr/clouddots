4097dd5927d140e5fe29da89b8b153c8a5453fa8
Mike Wallio <michael.wallio@outlook.com>
2024-09-25 13:00:55 -0400

    Configure code and some aliases

--- configgit.sh
+++ configgit.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-CODE=$(type code-insiders &>/dev/null && echo 'code-insiders -w' || echo 'code -w')
+# code works as the command even when using code-insiders.
+CODE='code -w'
 git config --global core.editor "$CODE"
 git config --global merge.tool vscode
-git config --global mergetool.vscode.cmd "$CODE \$MERGED"
+git config --global mergetool.vscode.cmd "$CODE --merge \$REMOTE \$LOCAL \$BASE \$MERGED"
 git config --global diff.tool vscode
 git config --global difftool.vscode.cmd "$CODE \$LOCAL \$REMOTE"
+git config --global alias.co checkout
+git config --global push.autoSetupRemote true
