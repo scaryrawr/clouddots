9eb14afc64a47dc145971a9dbdb6c32287489ce3
Mike Wallio <michael.wallio@outlook.com>
2024-09-25 10:17:38 -0400

    Moved tide.fish into configfish

--- tide.fish
+++
@@ -1,3 +0,0 @@
-#!/bin/env fish
-
-tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Fade --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Compact --icons='Many icons' --transient=Yes
