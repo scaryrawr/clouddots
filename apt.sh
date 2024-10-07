dc51a5fa232c66f2f79b5ba7dc2d68d24f0072a4
Mike Wallio <michael.wallio@outlook.com>
2024-10-07 16:44:48 -0400

    Add golang and install fzf from source

--- apt.sh
+++ apt.sh
@@ -2,26 +2,25 @@
 
 packages=("fish" "zsh" "ripgrep" "fzf" "zoxide")
 
-if command -v apt &> /dev/null
-then
+if command -v apt &>/dev/null; then
+	packages+=("golang-go")
 	# Enable apt-add-respository
 	sudo DEBIAN_FRONTEND="noninteractive" apt install software-properties-common -y
 
 	# Add fish shell repository
 	sudo DEBIAN_FRONTEND="noninteractive" apt-add-repository ppa:fish-shell/release-3 -y
-
+	sudo DEBIAN_FRONTEND="noninteractive" add-apt-repository ppa:longsleep/golang-backports -y
 	sudo DEBIAN_FRONTEND="noninteractive" apt update -y
 
 	# Install fish and other useful fun stuff
 	sudo DEBIAN_FRONTEND="noninteractive" apt install ${packages[@]} -y
-elif command -v dnf &> /dev/null
-then
+	go install github.com/junegunn/fzf@latest
+elif command -v dnf &>/dev/null; then
 	packages+=("eza")
 	sudo dnf update -y
 	sudo dnf upgrade -y
 	sudo dnf install ${packages[@]} -y
-elif  command -v brew &> /dev/null
-then
+elif command -v brew &>/dev/null; then
 	brew update
 	brew upgrade
 	brew install ${packages[@]}
@@ -29,4 +28,3 @@ else
 	echo "No package manager found"
 	exit 1
 fi
-
