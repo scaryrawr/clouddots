16bb98bc66d2b5651804a9912dd0000bca60e619
Mike Wallio <michael.wallio@outlook.com>
2024-10-23 15:24:54 -0400

    Move functions into plugin

--- configfish.sh
+++ configfish.sh
@@ -11,63 +11,7 @@ echo 'test -f $HOME/.cargo/env.fish && source $HOME/.cargo/env.fish' > $HOME/.co
 
 echo 'command -sq zoxide && zoxide init fish | source' > $HOME/.config/fish/conf.d/zoxide.fish
 
-echo 'function dotnet
-    if test -f /usr/local/bin/run-dotnet.sh
-        /usr/local/bin/run-dotnet.sh $argv
-    else
-        command dotnet $argv
-    end
-end' > $HOME/.config/fish/functions/dotnet.fish
-
-echo 'function nuget
-    if test -f /usr/local/bin/run-nuget.sh
-        /usr/local/bin/run-nuget.sh $argv
-    else
-        command nuget $argv
-    end
-end' > $HOME/.config/fish/functions/nuget.fish
-
-echo 'function npm
-    if test -f /usr/local/bin/run-npm.sh
-        /usr/local/bin/run-npm.sh $argv
-    else
-        command npm $argv
-    end
-end' > $HOME/.config/fish/functions/npm.fish
-
-echo 'function yarn
-    if test -f /usr/local/bin/run-yarn.sh
-        /usr/local/bin/run-yarn.sh $argv
-    else
-        command yarn $argv
-    end
-end' > $HOME/.config/fish/functions/yarn.fish
-
-echo 'function npx
-    if test -f /usr/local/bin/run-npx.sh
-        /usr/local/bin/run-npx.sh $argv
-    else
-        command npx $argv
-    end
-end' > $HOME/.config/fish/functions/npx.fish
-
-echo 'function rush
-    if test -f /usr/local/bin/run-rush.sh
-        /usr/local/bin/run-rush.sh $argv
-    else
-        command rush $argv
-    end
-end' > $HOME/.config/fish/functions/rush.fish
-
-echo 'function rush-pnpm
-    if test -f /usr/local/bin/run-rush-pnpm.sh
-        /usr/local/bin/run-rush-pnpm.sh $argv
-    else
-        command rush-pnpm $argv
-    end
-end' > $HOME/.config/fish/functions/rush-pnpm.fish
-
-fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher scaryrawr/tide jorgebucaran/autopair.fish </dev/null"
+fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher scaryrawr/tide scaryrawr/artifacts-helper.fish jorgebucaran/autopair.fish </dev/null"
 fish --command="tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Fade --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Compact --icons='Many icons' --transient=Yes"
 fish --command="command -q fzf && fisher install patrickf1/fzf.fish </dev/null"
 fish --command="command -q eza && fisher install scaryrawr/fish-eza </dev/null"
