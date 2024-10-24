a67f3ec9b05c4c9f7ce7d4f4eebb1c7c3137d594
Mike Wallio <michael.wallio@outlook.com>
2024-10-24 09:09:09 -0400

    Add replay

--- configfish.sh
+++ configfish.sh
@@ -11,7 +11,7 @@ echo 'test -f $HOME/.cargo/env.fish && source $HOME/.cargo/env.fish' > $HOME/.co
 
 echo 'command -sq zoxide && zoxide init fish | source' > $HOME/.config/fish/conf.d/zoxide.fish
 
-fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher scaryrawr/tide scaryrawr/artifacts-helper.fish jorgebucaran/autopair.fish </dev/null"
+fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher jorgebucaran/replay.fish scaryrawr/tide scaryrawr/artifacts-helper.fish jorgebucaran/autopair.fish </dev/null"
 fish --command="tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Fade --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Compact --icons='Many icons' --transient=Yes"
 fish --command="command -q fzf && fisher install patrickf1/fzf.fish </dev/null"
 fish --command="command -q eza && fisher install scaryrawr/fish-eza </dev/null"
