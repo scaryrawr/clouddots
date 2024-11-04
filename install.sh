2820ae5e18ea44cd30abde271845c0d5fc590242
Mike Wallio <michael.wallio@outlook.com>
2024-11-04 14:57:32 -0500

    Fix ordering of homebrew and apt

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 script_dir=$(dirname "$(readlink -f "$0")")
 
-$script_dir/apt.sh
-
 # Homebrew does not work on aarch64 linux
 [[ "$(uname -m)" != "aarch64" ]] && $script_dir/homebrew.sh
+
+$script_dir/apt.sh
 
 $script_dir/configzsh.sh
 $script_dir/configfish.sh
