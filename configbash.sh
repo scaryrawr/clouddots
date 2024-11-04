4ea7f5d0b0412491bd3d4d03bdbd0828fa37e6ef
Mike Wallio <michael.wallio@outlook.com>
2024-11-04 14:57:50 -0500

    Configure bash for homebrew

---
+++ configbash.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Just prepend to bashrc if it's not in it.
+prepend_entries=(
+	'[[ -f /home/linuxbrew/.linuxbrew/bin/brew ]] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+)
+
+for entry in "${prepend_entries[@]}"; do
+	if ! grep -q "$entry" "$HOME/.bashrc"; then
+		echo "$entry
+$(cat $HOME/.bashrc)" > $HOME/.bashrc
+	fi
+done
+
+# Just append to bashrc if it's not in it.
+append_entries=()
+
+for entry in "${append_entries[@]}"; do
+	if ! grep -q "$entry" "$HOME/.bashrc"; then
+		echo "$entry" >> $HOME/.bashrc
+	fi
+done
--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ script_dir=$(dirname "$(readlink -f "$0")")
 
 $script_dir/apt.sh
 
+$script_dir/configbash.sh
 $script_dir/configzsh.sh
 $script_dir/configfish.sh
 $script_dir/configgit.sh
