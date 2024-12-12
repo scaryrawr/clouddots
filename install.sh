b6e3d13dd9ef62722d141763bb97c473ce8ccce1
Mike Wallio <michael.wallio@outlook.com>
2024-12-11 19:29:08 -0500

    Add codespace check for running `configgit`

--- install.sh
+++ install.sh
@@ -10,5 +10,9 @@ $script_dir/apt.sh
 $script_dir/configbash.sh
 $script_dir/configzsh.sh
 $script_dir/configfish.sh
-# $script_dir/configgit.sh
+
+if [ "$CODESPACES" = "true" ]; then
+    $script_dir/configgit.sh
+fi
+
 cp $script_dir/p10k.zsh $HOME/.p10k.zsh
