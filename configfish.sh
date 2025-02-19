06507d8886574971f2fd5f4f109e2a9fa020ffa3
Mike Wallio <michael.wallio@outlook.com>
2025-02-19 15:47:55 -0500

    Add monorepo.fish

--- configfish.sh
+++ configfish.sh
@@ -13,7 +13,7 @@ echo 'command -sq zoxide && zoxide init fish | source' > "$HOME/.config/fish/con
 
 fish --command="curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher jorgebucaran/replay.fish scaryrawr/tide scaryrawr/artifacts-helper.fish jorgebucaran/autopair.fish </dev/null"
 fish --command="tide configure --auto --style=Rainbow --prompt_colors='16 colors' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Fade --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Compact --icons='Many icons' --transient=Yes"
-fish --command="command -q fzf && fisher install scaryrawr/fzf.fish </dev/null"
+fish --command="command -q fzf && fisher install scaryrawr/fzf.fish scaryrawr/monorepo.fish </dev/null"
 fish --command="command -q eza && fisher install scaryrawr/fish-eza </dev/null"
 test -f "$NVM_DIR/nvm.sh" && fish --command="fisher install scaryrawr/codespace-nvm.fish </dev/null"
 fish --command="set -Ux EZA_STANDARD_OPTIONS --icons"
