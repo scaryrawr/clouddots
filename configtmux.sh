bafab35782827315812ee5543a6e2cf7ab3b8411
Mike Wallio <michael.wallio@outlook.com>
2025-03-26 10:12:14 -0400

    Customize status bar a little

--- configtmux.sh
+++ configtmux.sh
@@ -8,4 +8,16 @@ set -g mouse on
 set-option -sg escape-time 10
 set-option -g focus-events on
 set-option -sa terminal-features ',*:RGB'
+set -g default-terminal "tmux-256color"
+
+# status bar
+set -g status-style bg=default
+
+set -g status-left "#H - Session #S"
+
+set -g status-justify centre
+set -g window-status-format "#I: #W"
+set -g window-status-current-style "underscore"
+
+set-option -g status-right "%a %d %b %l:%M %p"
 EOF
