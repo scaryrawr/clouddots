216227a35d4dd1fb3ff66e007ed1b089e9f18d84
Mike Wallio <michael.wallio@outlook.com>
2025-07-09 20:01:02 -0400

    Set maxRequests to over 9000

--- configvscode.sh
+++ configvscode.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # Enable yolo mode by default in dev containers
-jq '. + {"chat.tools.autoApprove": true}' "$HOME/.vscode-remote/data/Machine/settings.json" > tmp.json 
+jq '. + {"chat.tools.autoApprove": true}' "$HOME/.vscode-remote/data/Machine/settings.json" >tmp.json
+mv tmp.json "$HOME/.vscode-remote/data/Machine/settings.json"
+
+# Power level over 9000
+jq '. + {"chat.agent.maxRequests": 9001}' "$HOME/.vscode-remote/data/Machine/settings.json" >tmp.json
 mv tmp.json "$HOME/.vscode-remote/data/Machine/settings.json"
