facf3371cf1ae4f4938db44a57131728cdbe673f
Mike Wallio <michael.wallio@outlook.com>
2025-07-10 22:28:03 -0400

    Attempt to set multiple settings at once

--- configvscode.sh
+++ configvscode.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-# Enable yolo mode by default in dev containers
-jq '. + {"chat.tools.autoApprove": true}' "$HOME/.vscode-remote/data/Machine/settings.json" >tmp.json
+jq '. + {
+  "chat.tools.autoApprove": true,
+  "chat.agent.maxRequests": 9001,
+  "chat.editing.alwaysSaveWithGeneratedChanges": true,
+  "github.copilot.chat.codesearch.enabled": true,
+  "github.copilot.chat.editor.temporalContext.enabled": true,
+  "github.copilot.chat.edits.temporalContext.enabled": true,
+  "github.copilot.nextEditSuggestions.enabled": true,
+  "github.copilot.chat.generateTests.codeLens": true,
+  "github.copilot.chat.languageContext.typescript.enabled": true,
+  "github.copilot.chat.search.semanticTextResults": true,
+  "github.copilot.chat.completionContext.typescript.mode": "on",
+  "github.copilot.chat.languageContext.fix.typescript.enabled": true,
+  "github.copilot.chat.languageContext.inline.typescript.enabled": true,
+  "github.copilot.nextEditSuggestions.fixes": true,
+  "github.copilot.chat.agent.thinkingTool": true,
+  "chat.edits2.enabled": true,
+  "inlineChat.enableV2": true,
+  "chat.agent.maxRequests": 9001,
+}' "$HOME/.vscode-remote/data/Machine/settings.json" >tmp.json
 mv tmp.json "$HOME/.vscode-remote/data/Machine/settings.json"
 
-# Power level over 9000
-jq '. + {"chat.agent.maxRequests": 9001}' "$HOME/.vscode-remote/data/Machine/settings.json" >tmp.json
-mv tmp.json "$HOME/.vscode-remote/data/Machine/settings.json"
