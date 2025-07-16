ffd4e1ef2cd51d1215ea575947f1460a46ac6744
Mike Wallio <michael.wallio@outlook.com>
2025-07-15 20:29:28 -0400

    Add unzip

--- system-deps.sh
+++ system-deps.sh
@@ -63,6 +63,7 @@ elif command -v dnf &>/dev/null; then
   if [ ${#packages[@]} -ne 0 ]; then
     sudo dnf update -y
     sudo dnf install --skip-unavailable "${packages[@]}" -y
+    sudo dnf install -y git zsh gcc gcc-c++ which wget unzip
   fi
 
 else
