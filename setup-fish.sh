8380c361dddac19a5be63339bdab2a87d41c9e2c
Mike Wallio <michael.wallio@outlook.com>
2025-09-23 22:02:48 -0400

    Disable tmux autostart

--- setup-fish.sh
+++ setup-fish.sh
@@ -41,7 +41,7 @@ fish --command="tide configure --auto --style=Rainbow --prompt_colors='16 colors
 fish --command="command -q zoxide && fisher install scaryrawr/zoxide.fish </dev/null"
 fish --command="command -q fzf && fisher install scaryrawr/fzf.fish scaryrawr/monorepo.fish </dev/null"
 fish --command="command -q eza && fisher install scaryrawr/fish-eza </dev/null"
-fish --command="command -q tmux && fisher install scaryrawr/tmux.fish </dev/null && set -Ux TMUX_POWERLINE_BUBBLE_SEPARATORS true && set -Ux TMUX_SSHAUTO_START true"
+fish --command="command -q tmux && fisher install scaryrawr/tmux.fish </dev/null && set -Ux TMUX_POWERLINE_BUBBLE_SEPARATORS true && set -Ux TMUX_SSHAUTO_START false"
 fish --command="set -Ux EZA_STANDARD_OPTIONS --icons"
 
 sudo chsh -s $(which fish) $(whoami)
