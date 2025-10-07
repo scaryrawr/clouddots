3814d2fa6dd069513c3f9c851fb15872f82dda95
Mike Wallio <michael.wallio@outlook.com>
2025-10-07 11:31:11 -0400

    Add p10k-ext

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -33,6 +33,7 @@ plugins=(
   "https://github.com/scaryrawr/fzf.zsh $ZSH_CUSTOM/plugins/fzf"
   "https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab"
   "https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions"
+  "https://github.com/scaryrawr/p10k-ext $ZSH_CUSTOM/plugins/p10k-ext"
 )
 
 # Clone or pull each plugin
@@ -49,7 +50,7 @@ done
 
 sed -i 's/ZSH_THEME=\(.*\)/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$HOME/.zshrc"
 
-sed -i 's/plugins=\(.*\)/plugins=(gh fast-syntax-highlighting yarn zfunctions zsh-autosuggestions zsh-completions zsh-autopair zoxide fzf-tab fzf eza tmux)/' "$HOME/.zshrc"
+sed -i 's/plugins=\(.*\)/plugins=(gh fzf p10k-ext fast-syntax-highlighting yarn zfunctions zsh-autosuggestions zsh-completions zsh-autopair zoxide fzf-tab eza tmux)/' "$HOME/.zshrc"
 
 # Just append to zshrc if it's not in it.
 append_entries=(
