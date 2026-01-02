4e0ac614355ae447f456d5a5fddb812941057b6d
Mike Wallio <michael.wallio@outlook.com>
2026-01-02 03:09:46 +0000

    Setup claude and personal marketplace

---
+++ setup-claude.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+claude plugin marketplace add scaryrawr/scaryrawr-plugins
+
+claude plugin install typescript-native-lsp
+claude plugin install chrome-devtools
+claude plugin install azure-cli
