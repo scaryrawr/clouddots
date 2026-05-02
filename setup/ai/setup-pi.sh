94082a2f22d494a8ec04da3ad137770b6590cd54
Mike Wallio <michael.wallio@outlook.com>
2026-05-01 21:19:35 -0400

    Add webfetch to pi (#134)

--- setup/ai/setup-pi.sh
+++ setup/ai/setup-pi.sh
@@ -2,4 +2,5 @@
 set -e
 pi install git:github.com/scaryrawr/pi-lmstudio-ollama
 pi install git:github.com/scaryrawr/pi-automode
+pi install git:github.com/scaryrawr/pi-webfetch
 pi install git:github.com/scaryrawr/scarypilot
