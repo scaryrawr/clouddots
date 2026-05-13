f566a78e00865c50529253149ffcb31351ccded0
Mike Wallio <michael.wallio@outlook.com>
2026-05-12 21:55:18 -0400

    Add autoresearch (#138)

--- setup/ai/setup-pi.sh
+++ setup/ai/setup-pi.sh
@@ -6,3 +6,4 @@ pi install git:github.com/scaryrawr/pi-lmstudio-ollama
 pi install git:github.com/scaryrawr/pi-automode
 pi install git:github.com/scaryrawr/pi-webfetch
 pi install git:github.com/scaryrawr/scarypilot
+pi install git:github.com/davebcn87/pi-autoresearch
