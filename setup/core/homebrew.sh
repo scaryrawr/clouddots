675a3388ebe2ecf1e20ed0318f1f70bc3c5e0913
Mike Wallio <michael.wallio@outlook.com>
2026-08-03 09:22:15 -0400

    Fix homebrew blockers

--- setup/core/homebrew.sh
+++ setup/core/homebrew.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Homebrew refuses to update its download trust store when this directory is
+# group-writable, which can be inherited from Codespaces' default umask.
+mkdir -p "$HOME/.homebrew"
+chmod go-w "$HOME/.homebrew"
+
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Ensure brew is on PATH for the rest of this script
@@ -11,6 +16,12 @@ elif [[ -x /opt/homebrew/bin/brew ]]; then
 elif [[ -x /usr/local/bin/brew ]]; then
   eval "$(/usr/local/bin/brew shellenv)"
 fi
+
+# Homebrew may use Ubuntu's system Git, which does not support the global
+# `zdiff3` setting used by newer Git versions.
+export GIT_CONFIG_COUNT=1
+export GIT_CONFIG_KEY_0=merge.conflictstyle
+export GIT_CONFIG_VALUE_0=diff3
 
 brew install -y \
   ast-grep \
